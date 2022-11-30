59ddcb00f6887e11aabf40111e1ce93262a39970
EduardSekan <sekaneduard@gmail.com>
2022-11-30 21:26:41 +0200

    add hw - 1

--- src/components/Feedback/Feedback.jsx
+++ src/components/Feedback/Feedback.jsx
@@ -1,12 +1,57 @@
 import React, { Component } from 'react';
+import { Statistics } from './Statistics';
 class Feedback extends Component {
   state = {
     good: 0,
     neutral: 0,
-    bad: 0
-  }
+    bad: 0,
+  };
+
+  handleClickGood = () => {
+    this.setState(prevState => ({
+      good: prevState.good + 1,
+    }));
+  };
+  handleCLickNeutral = () => {
+    this.setState(prevState => ({
+      neutral: prevState.neutral + 1,
+    }));
+  };
+  handleCLickBad = () => {
+    this.setState(prevState => ({
+      bad: prevState.bad + 1,
+    }));
+  };
+
+  countTotalFeedback = (initial) => {
+    const { good, neutral, bad } = this;
+    let total = initial;
+    return (total += good + neutral + bad);
+  };
+
+  // countPositiveFeedbackPercentage = () => {};
+
   render() {
-    return <div></div>;
+    return (
+      <div>
+        <h2> Please leave feedback</h2>
+        <button type="button" onClick={this.handleClickGood}>
+          Good
+        </button>
+        <button type="button" onClick={this.handleCLickNeutral}>
+          Neutral
+        </button>
+        <button type="button" onClick={this.handleCLickBad}>
+          Bad
+        </button>
+        <Statistics
+          good={this.state.good}
+          neutral={this.state.neutral}
+          bad={this.state.bad}
+          total={this.countTotalFeedback()}
+        />
+      </div>
+    );
   }
 }
 
@@ -17,11 +62,8 @@ export default Feedback;
 
 // Состояние приложения обязательно должно быть следующего вида, добавлять новые свойства нельзя.
 
-
 // Шаг 2
 // Расширь функционал приложения так, чтобы в интерфейсе отображалось больше статистики о собранных отзывах. Добавь отображение общего количества собранных отзывов из всех категорий и процент положительных отзывов. Для этого создай вспомогательные методы countTotalFeedback() и countPositiveFeedbackPercentage(), подсчитывающие эти значения основываясь на данных в состоянии (вычисляемые данные).
-
-
 
 // Шаг 3
 // Выполни рефакторинг приложения. Состояние приложения должно оставаться в корневом компоненте <App>.
---
+++ src/components/Feedback/Statistics.jsx
@@ -0,0 +1,13 @@
+export const Statistics = ({ good, neutral, bad, total }) => {
+  return (
+    <div className="Statistics">
+      <h2>Statistics</h2>
+      <ul>
+        <li>Good: {good}</li>
+        <li>Neutral: {neutral}</li>
+        <li>Bad: {bad}</li>
+        <li>Total: {total}</li>
+      </ul>
+    </div>
+  );
+};
